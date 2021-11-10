a9845fb9b32afedaee8694b0f14c76b9aa62cd7b
Fernando Biggs <fbiggs51@gmail.com>
2021-11-10 08:58:05 -0600

    actualizacion carpeta (aparecia vacia en github)

---
+++ Modulos/ComprasCP/Requerimiento Individual - Fernando Biggs/AreaComprasyCxP/CapaControladorCompras/clscontrolador.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.Odbc;
+using CapaModeloCompras;
+
+namespace CapaControladorCompras
+{
+    public class clscontrolador
+    {
+        //Instanciamos la clase sentencias
+
+        clssentencias sn = new clssentencias();
+                public OdbcDataReader insertar_bodegas(string[] dato)
+                {
+                    return sn.insertar_bodegas(dato);
+                }
+                public OdbcDataReader insertar_linea(string[] dato)
+                {
+                    return sn.insertar_linea(dato);
+                }
+                public OdbcDataReader insertar_marca(string[] dato)
+                {
+                    return sn.insertar_marca(dato);
+                }
+
+        //fernando biggs
+        public OdbcDataReader insertar_OrdenC(string[] dato)
+        {
+            return sn.insertar_OrdenCompra(dato);
+        }
+
+        public OdbcDataReader llenarcboProv()
+        {
+            return sn.llenarcboprovCompras();
+        }
+
+        public OdbcDataReader insertar_FacturaEn(string[] dato)
+        {
+            return sn.insertar_FacturaEncabezado(dato);
+        }
+
+        public OdbcDataReader llenarcboOrdenC()
+        {
+            return sn.llenarcboIDorden();
+        }
+
+        public OdbcDataReader insertar_FacturaD(string[] dato)
+        {
+            return sn.insertar_FacturaDetalle(dato);
+        }
+
+        public OdbcDataReader llenarFacturasE()
+        {
+            return sn.llenarFacE();
+        }
+
+        public OdbcDataReader llenarProducto()
+        {
+            return sn.llenarPr();
+        }
+
+        public DataTable llenarDvgOrdenCompra(string condicion)
+        {
+            OdbcDataAdapter dt = sn.llenardvgor(condicion);
+            DataTable table = new DataTable();
+            dt.Fill(table);
+            return table;
+        }
+
+        public DataTable llenarDvgFE(string condicion)
+        {
+            OdbcDataAdapter dt = sn.llenardvgFE(condicion);
+            DataTable table = new DataTable();
+            dt.Fill(table);
+            return table;
+        }
+
+        public DataTable actualizar()
+        {
+            OdbcDataAdapter dt = sn.llenardvgORNOR();
+            DataTable table = new DataTable();
+            dt.Fill(table);
+            return table;
+        }
+        public DataTable actualizarFE()
+        {
+            OdbcDataAdapter dt = sn.llenardvgFEnc();
+            DataTable table = new DataTable();
+            dt.Fill(table);
+            return table;
+        }
+
+        public OdbcDataReader llenarProv()
+        {
+            return sn.llenarPrOr();
+        }
+
+        public DataTable actualizar2()
+        {
+            OdbcDataAdapter dt = sn.llenardvgFD();
+            DataTable table = new DataTable();
+            dt.Fill(table);
+            return table;
+        }
+
+        public DataTable llenarDvgFdetalle(string condicion)
+        {
+            OdbcDataAdapter dt = sn.llenardvgFde(condicion);
+            DataTable table = new DataTable();
+            dt.Fill(table);
+            return table;
+        }
+    }
+}
---
+++ Modulos/ComprasCP/Requerimiento Individual - Fernando Biggs/AreaComprasyCxP/CapaModeloCompras/clssentencias.cs
@@ -0,0 +1,252 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Odbc;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaModeloCompras
+{
+    public class clssentencias
+    {
+        clsconexion cn = new clsconexion(); //crear objeto
+        OdbcCommand com; //variable para querys
+
+        public OdbcDataReader insertar_bodegas(string[] datos) //funcion para insertar en db
+        {
+            try
+            {
+                cn.conexion();
+                string consulta = "INSERT INTO bodegas Values ('" + datos[0] + "','" + datos[1] + "','" + datos[2] + "');";
+                com = new OdbcCommand(consulta, cn.conexion());
+                OdbcDataReader respuesta = com.ExecuteReader();
+                return respuesta;
+            }
+            catch (Exception err)
+            {
+                Console.WriteLine(err.Message);
+                return null;
+            }
+        }
+
+        public OdbcDataReader insertar_linea(string[] datos) //funcion para insertar en db
+        {
+            try
+            {
+                cn.conexion();
+                string consulta = "INSERT INTO linea Values ('" + datos[0] + "','" + datos[1] + "','" + datos[2] + "','" + datos[3] + "');";
+                com = new OdbcCommand(consulta, cn.conexion());
+                OdbcDataReader respuesta = com.ExecuteReader();
+                return respuesta;
+            }
+            catch (Exception err)
+            {
+                Console.WriteLine(err.Message);
+                return null;
+            }
+        }
+
+        public OdbcDataReader insertar_marca(string[] datos) //funcion para insertar en db
+        {
+            try
+            {
+                cn.conexion();
+                string consulta = "INSERT INTO marca Values ('" + datos[0] + "','" + datos[1] + "','" + datos[2] + "','" + datos[3] + "');";
+                com = new OdbcCommand(consulta, cn.conexion());
+                OdbcDataReader respuesta = com.ExecuteReader();
+                return respuesta;
+            }
+            catch (Exception err)
+            {
+                Console.WriteLine(err.Message);
+                return null;
+            }
+        }
+
+        // FERNANDO BIGGS INSERTS
+        public OdbcDataReader insertar_OrdenCompra(string[] datos)
+        {
+            try
+            {
+                cn.conexion();
+                string consulta = "INSERT INTO ordencompra Values ('" + datos[0] + "','" + datos[1] + "','" + datos[2] + "','" + datos[3] + "','" + datos[4] + "');";
+                com = new OdbcCommand(consulta, cn.conexion());
+                OdbcDataReader respuesta = com.ExecuteReader();
+                return respuesta;
+            }
+            catch (Exception err)
+            {
+                Console.WriteLine(err.Message);
+                return null;
+            }
+        }
+
+        public OdbcDataReader insertar_FacturaEncabezado(string[] datos)
+        {
+            try
+            {
+                cn.conexion();
+                string consulta = "INSERT INTO facturaencabezado Values ('" + datos[0] + "','" + datos[1] + "','" + datos[2] + "','" + datos[3] + "','" + datos[4] + "','" + datos[5] + "');";
+                com = new OdbcCommand(consulta, cn.conexion());
+                OdbcDataReader respuesta = com.ExecuteReader();
+                return respuesta;
+            }
+            catch (Exception err)
+            {
+                Console.WriteLine(err.Message);
+                return null;
+            }
+        }
+
+        public OdbcDataReader insertar_FacturaDetalle(string[] datos)
+        {
+            try
+            {
+                cn.conexion();
+                string consulta = "INSERT INTO facturadetalle Values ('" + datos[0] + "','" + datos[1] + "','" + datos[2] + "','" + datos[3] + "','" + datos[4] + "');";
+                com = new OdbcCommand(consulta, cn.conexion());
+                OdbcDataReader respuesta = com.ExecuteReader();
+                return respuesta;
+            }
+            catch (Exception err)
+            {
+                Console.WriteLine(err.Message);
+                return null;
+            }
+        }
+
+        public OdbcDataReader llenarcboprovCompras()
+        {
+            try
+            {
+                string sql = "select idProveedor, nombre from proveedor;";
+                OdbcCommand datos = new OdbcCommand(sql, cn.conexion());
+                OdbcDataReader leer = datos.ExecuteReader();
+                return leer;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return null;
+            }
+
+        }
+
+        public OdbcDataReader llenarcboIDorden()
+        {
+            try
+            {
+                string sql = "SELECT ordencompra.idOrdenCompra, ordencompra.idProveedor,proveedor.nombre, ordencompra.total, ordencompra.fecha, ordencompra.descripcion FROM ordencompra INNER JOIN proveedor ON proveedor.idProveedor = ordencompra.idProveedor";
+                OdbcCommand datos = new OdbcCommand(sql, cn.conexion());
+                OdbcDataReader leer = datos.ExecuteReader();
+                return leer;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return null;
+            }
+
+
+
+        }
+
+        public OdbcDataReader llenarFacE()
+        {
+            try
+            {
+                string sql = "SELECT idFacturaEncabezado from facturaencabezado";
+                OdbcCommand datos = new OdbcCommand(sql, cn.conexion());
+                OdbcDataReader leer = datos.ExecuteReader();
+                return leer;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return null;
+            }
+        }
+        public OdbcDataReader llenarPr()
+        {
+            try
+            {
+                string sql = "SELECT pkid,Nombre from producto";
+                OdbcCommand datos = new OdbcCommand(sql, cn.conexion());
+                OdbcDataReader leer = datos.ExecuteReader();
+                return leer;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return null;
+            }
+        }
+
+        public OdbcDataAdapter llenartb2()
+        {
+            string sql = "select * from ordencompra;";
+            OdbcDataAdapter datatable = new OdbcDataAdapter(sql, cn.conexion());
+            return datatable;
+        }
+
+        public OdbcDataReader llenarPrOr()
+        {
+            try
+            {
+                string sql = "select idProveedor, nombre from proveedor;";
+                OdbcCommand datos = new OdbcCommand(sql, cn.conexion());
+                OdbcDataReader leer = datos.ExecuteReader();
+                return leer;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return null;
+            }
+
+        }
+        public OdbcDataAdapter llenardvgor(string condicion)
+        {
+            string sql = "SELECT ordencompra.idOrdenCompra, ordencompra.idProveedor, ordencompra.total, ordencompra.fecha, ordencompra.descripcion FROM ordencompra where ordencompra.idProveedor=" + '"' + condicion + '"' + ";";
+            OdbcDataAdapter datatable = new OdbcDataAdapter(sql, cn.conexion());
+            return datatable;
+        }
+
+        public OdbcDataAdapter llenardvgFE(string condicion)
+        {
+            string sql = "SELECT * FROM facturaencabezado where idProveedor =" + '"' + condicion + '"' + ";";
+            OdbcDataAdapter datatable = new OdbcDataAdapter(sql, cn.conexion());
+            return datatable;
+        }
+
+        public OdbcDataAdapter llenardvgORNOR()
+        {
+            string sql = "SELECT * from ordencompra";
+            OdbcDataAdapter datatable = new OdbcDataAdapter(sql, cn.conexion());
+            return datatable;
+        }
+
+        public OdbcDataAdapter llenardvgFEnc()
+        {
+            string sql = "SELECT * from facturaencabezado";
+            OdbcDataAdapter datatable = new OdbcDataAdapter(sql, cn.conexion());
+            return datatable;
+        }
+
+        public OdbcDataAdapter llenardvgFD()
+        {
+            string sql = "SELECT * from facturadetalle";
+            OdbcDataAdapter datatable = new OdbcDataAdapter(sql, cn.conexion());
+            return datatable;
+        }
+
+        public OdbcDataAdapter llenardvgFde(string condicion)
+        {
+            string sql = "SELECT * FROM facturadetalle where idProducto =" + '"' + condicion + '"' + ";";
+            OdbcDataAdapter datatable = new OdbcDataAdapter(sql, cn.conexion());
+            return datatable;
+        }
+
+    }
+}
