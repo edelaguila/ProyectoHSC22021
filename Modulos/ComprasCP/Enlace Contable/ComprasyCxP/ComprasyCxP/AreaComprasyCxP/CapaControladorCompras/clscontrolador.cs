b4e017f2b59df0eda02176a1126ff278c3032b9c
Arnol Veliz <arnolveliz10@gmail.com>
2021-11-09 23:48:47 -0600

    Requerimiento individual, Enlace contable, Arnol Veliz

---
+++ Modulos/ComprasCP/Enlace Contable/ComprasyCxP/ComprasyCxP/AreaComprasyCxP/CapaControladorCompras/clscontrolador.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.Odbc;
+using CapaModeloCompras;
+
+namespace CapaControladorCompras
+{
+    public class clscontrolador
+    {
+        //Instanciamos la clase sentencias
+
+        clssentencias sn = new clssentencias();
+                public OdbcDataReader insertar_bodegas(string[] dato)
+                {
+                    return sn.insertar_bodegas(dato);
+                }
+                public OdbcDataReader insertar_linea(string[] dato)
+                {
+                    return sn.insertar_linea(dato);
+                }
+                public OdbcDataReader insertar_marca(string[] dato)
+                {
+                    return sn.insertar_marca(dato);
+                }
+
+        //fernando biggs
+        public OdbcDataReader insertar_OrdenC(string[] dato)
+        {
+            return sn.insertar_OrdenCompra(dato);
+        }
+
+        public OdbcDataReader llenarcboProv()
+        {
+            return sn.llenarcboprovCompras();
+        }
+
+        public OdbcDataReader insertar_FacturaEn(string[] dato)
+        {
+            return sn.insertar_FacturaEncabezado(dato);
+        }
+
+        public OdbcDataReader llenarcboOrdenC()
+        {
+            return sn.llenarcboIDorden();
+        }
+
+        public OdbcDataReader insertar_FacturaD(string[] dato)
+        {
+            return sn.insertar_FacturaDetalle(dato);
+        }
+
+        public OdbcDataReader llenarFacturasE()
+        {
+            return sn.llenarFacE();
+        }
+
+        public OdbcDataReader llenarProducto()
+        {
+            return sn.llenarPr();
+        }
+
+        public DataTable llenarDvgOrdenCompra(string condicion)
+        {
+            OdbcDataAdapter dt = sn.llenardvgor(condicion);
+            DataTable table = new DataTable();
+            dt.Fill(table);
+            return table;
+        }
+
+        public DataTable llenarDvgFE(string condicion)
+        {
+            OdbcDataAdapter dt = sn.llenardvgFE(condicion);
+            DataTable table = new DataTable();
+            dt.Fill(table);
+            return table;
+        }
+
+        public DataTable actualizar()
+        {
+            OdbcDataAdapter dt = sn.llenardvgORNOR();
+            DataTable table = new DataTable();
+            dt.Fill(table);
+            return table;
+        }
+        public DataTable actualizarFE()
+        {
+            OdbcDataAdapter dt = sn.llenardvgFEnc();
+            DataTable table = new DataTable();
+            dt.Fill(table);
+            return table;
+        }
+
+        public OdbcDataReader llenarProv()
+        {
+            return sn.llenarPrOr();
+        }
+
+        public DataTable actualizar2()
+        {
+            OdbcDataAdapter dt = sn.llenardvgFD();
+            DataTable table = new DataTable();
+            dt.Fill(table);
+            return table;
+        }
+
+        public DataTable llenarDvgFdetalle(string condicion)
+        {
+            OdbcDataAdapter dt = sn.llenardvgFde(condicion);
+            DataTable table = new DataTable();
+            dt.Fill(table);
+            return table;
+        }
+
+        //arnolGOOOOD
+
+        public string calculoDebe(string fechaInicio, string fechaFinal)
+        {
+            string total = sn.calculoDebe(fechaInicio, fechaFinal);
+            return total;
+        }
+
+        public string calculoHaber(string fechaInicio, string fechaFinal)
+        {
+            string total = sn.calculoHaber(fechaInicio, fechaFinal);
+            return total;
+        }
+
+        //Insertar encabezado
+        public void insertarEncabezado(string id, string fecha, string tipoPoliza)
+        {
+            sn.insertarEncabezado(id, fecha, tipoPoliza);
+
+        }
+        public string incrementarId()
+        {
+            string id = sn.incrementarId();
+
+            return id;
+        }
+
+        public void insertarDetalle(string Id, string fecha, string idCuenta, string saldo, string idtipoOp, string concepto)
+        {
+            sn.insertarDetalle(Id, fecha, idCuenta, saldo, idtipoOp, concepto);
+        }
+
+
+    }
+}
---
+++ Modulos/ComprasCP/Enlace Contable/ComprasyCxP/ComprasyCxP/AreaComprasyCxP/CapaVistaCompras/Area_Compras.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CapaVistaSeguridadHSC;
+using CapaVista;
+using CapaVistaReporte;
+
+namespace CapaVistaCompras
+{
+    public partial class Area_Compras : Form
+    {
+
+        public Area_Compras()
+        {
+            InitializeComponent();
+        }
+
+        private void Area_Compras_Load(object sender, EventArgs e)
+        {
+            this.Hide();
+            frmLoginHSC form = new frmLoginHSC();
+            if (form.ShowDialog() == DialogResult.OK)
+            {
+                txtusuario.Text = form.usuario();
+                this.Show();
+            }
+            else
+            {
+                this.Close();
+            }
+        }
+
+        private void cerrarSesiónToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            frmLoginHSC form = new frmLoginHSC();
+            if (form.ShowDialog() == DialogResult.OK)
+            {
+                txtusuario.Text = form.usuario();
+                this.Show();
+            }
+            else
+            {
+                this.Close();
+                txtusuario.Text = "";
+                this.Close();
+            }
+
+        }
+
+        private void salirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void consultasInteligentesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            menuconsultas formulario = new menuconsultas();
+            formulario.Show();
+        }
+
+        private void reporteadorToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ReporteadorAdmin formulario = new ReporteadorAdmin();
+            formulario.MdiParent = this;
+            formulario.Show();
+        }
+
+        private void moduloDeSeguridadToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frmMIDSeguridad formulario = new frmMIDSeguridad();
+            formulario.Show();
+        }
+
+        private void ingresarOrdenDeCompraToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FrmCompra formulario = new FrmCompra();
+            formulario.MdiParent = this;
+            formulario.Show();
+        }
+
+        private void buscarOrdenDeCompraToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frmOrdenCompraBUSQUEDA formulario = new frmOrdenCompraBUSQUEDA();
+            formulario.MdiParent = this;
+            formulario.Show();
+        }
+
+        private void facturasEncabezadoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ingresarFacturasEncabezadoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frmFacturaEncabezado formulario = new frmFacturaEncabezado();
+            formulario.MdiParent = this;
+            formulario.Show();
+        }
+
+        private void buscarFacturasEncabezadoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frmFacturaEncabezadoBUSQUEDA formulario = new frmFacturaEncabezadoBUSQUEDA();
+            formulario.MdiParent = this;
+            formulario.Show();
+        }
+
+        private void ingresarFacturaDetalleToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frmFacturaDetalle formulario = new frmFacturaDetalle();
+            formulario.MdiParent = this;
+            formulario.Show();
+        }
+
+        private void buscarFacturaDetalleToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frmFacturaDetalleBUSQUEDA formulario = new frmFacturaDetalleBUSQUEDA();
+            formulario.MdiParent = this;
+            formulario.Show();
+        }
+
+
+
+        private void ordenesDeCompraYFacturasToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frmAyudasOrdenesCompra formulario = new frmAyudasOrdenesCompra();
+            formulario.MdiParent = this;
+            formulario.Show();
+        }
+
+        private void ordenesDeCompraToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            FrmReportesOrdenCompra form = new FrmReportesOrdenCompra();
+            form.MdiParent = this;
+            form.Show();
+        }
+
+        private void enlaceContableToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frmEnlaceContableCompras form = new frmEnlaceContableCompras();
+            form.MdiParent = this;
+            form.Show();
+        }
+    }
+}
